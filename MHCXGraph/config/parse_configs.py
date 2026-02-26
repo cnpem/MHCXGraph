dae5836e9f7a09294ca2405bf1fedeafaafb1554
jvsguerra <jvsguerra@gmail.com>
2026-02-26 19:03:20 -0300

    Format file

--- MHCXGraph/config/parse_configs.py
+++ MHCXGraph/config/parse_configs.py
@@ -8,16 +8,19 @@ from graphein.protein.features.nodes.dssp import rsa, secondary_structure
 
 def make_graph_config(centroid_threshold):
     return ProteinGraphConfig(
-        edge_construction_functions=[partial(add_distance_threshold, long_interaction_threshold=0, threshold=centroid_threshold)],
+        edge_construction_functions=[
+            partial(
+                add_distance_threshold,
+                long_interaction_threshold=0,
+                threshold=centroid_threshold,
+            )
+        ],
         graph_metadata_functions=[rsa, secondary_structure],
         dssp_config=DSSPConfig(),
         granularity="centroids",
     )
 
+
 def parse_serd_config(json_path: str):
     with open(json_path) as f:
         data = json.load(f)
-
-
-
-
